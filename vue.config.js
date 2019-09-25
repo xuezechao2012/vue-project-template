93cea4b8df18e0c4d57dec398608e0729fc154c0
wuyiya <1312933886@qq.com>
2019-09-25 21:55:26 +0800

    引入lodash

--- vue.config.js
+++ vue.config.js
@@ -1,19 +1,10 @@
 const path = require('path')
 
+const LodashModuleReplacementPlugin = require('lodash-webpack-plugin')
 module.exports = {
   // 打包输出文件夹名字
-  outputDir: 'docs',
-  // 预加载配置
-  configureWebpack: () => {
-    if (process.env.NODE_ENV !== 'production') return
-    return {
-      plugins: []
-    }
-  },
+  publicPath: '/',
+  outputDir: 'dist',
   // 关闭打包生成.map文件
-  productionSourceMap: false,
-  // 第三方插件配置
-  pluginOptions: {
-    // ...
-  }
+  productionSourceMap: false
 }
