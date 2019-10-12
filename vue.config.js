206cb03d38971c5c9b3da37a77866f0e76dc5651
leiyalin <leiyalin@htbaobao.com>
2019-10-12 11:51:39 +0800

    add gzip

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,8 @@
 'use strict'
 const path = require('path')
-// const isProduction = process.env.NODE_ENV === 'production'
+const CompressionWebpackPlugin = require('compression-webpack-plugin') // gzip压缩
+const productionGzipExtensions = /\.(js|css|json|txt|html|ico|svg)(\?.*)?$/i // gzip匹配文件规则
+const isProduction = process.env.NODE_ENV === 'production'
 
 function resolve(dir) {
   return path.join(__dirname, dir)
@@ -51,5 +53,21 @@ module.exports = {
         }
       }
     })
+  },
+  configureWebpack: config => {
+    const plugins = []
+    if (isProduction) {
+      plugins.push(
+        new CompressionWebpackPlugin({
+          filename: '[path].gz[query]',
+          algorithm: 'gzip',
+          test: productionGzipExtensions,
+          threshold: 10240, // 只处理比这个值大的资源，按字节计算
+          minRatio: 0.8, // 只有压缩率比这个值小的资源才会被处理
+          deleteOriginalAssets: false //是否删除原始资源 默认false
+        })
+      )
+    }
+    config.plugins = [...config.plugins, ...plugins]
   }
 }
